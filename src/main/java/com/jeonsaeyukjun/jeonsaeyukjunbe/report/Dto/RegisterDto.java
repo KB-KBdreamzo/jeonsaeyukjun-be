5b72085c75b66b9c2d03e26e394223beadcab922
yoondabin <yundabin0608@gmail.com>
2024-09-27 11:47:47 +0900

    feat: KBD-7 Dto 추가 및 그에따른 수정

---
+++ src/main/java/com/jeonsaeyukjun/jeonsaeyukjunbe/report/Dto/RegisterDto.java
@@ -0,0 +1,30 @@
+package com.jeonsaeyukjun.jeonsaeyukjunbe.report.Dto;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+public class RegisterDto {
+    private String lessorName;                   // 임대인 이름
+    private String roadName;                     // 도로명 주소
+    private String detailAddress;                // 상세 주소
+
+    // 토지 관련
+    private String landType;
+    private double landArea;
+    private String buildingType;
+    private double buildingArea;
+
+    private boolean auctionRecord;               // 경매 기록 여부
+    private boolean injuctionRecord;             // 가처분 기록 여부
+    private boolean trustRegistrationRecord;     // 신탁 설정 여부
+    private boolean redemptionRecord;            // 환매 특약 여부
+    private boolean registrationRecord;          // 가등기 여부
+    private int seizureCount;                    // 압류 내역 개수
+    private int provisionalSeizureCount;         // 가압류 내역 개수
+
+    // 을구
+    private long priorityDeposit;              // 선순위채권총액
+    private int leaseholdRegistrationCount;      // 전세권 설정 내역 개수
+    private int mortgageCount;                   // 근저당 설정 내역 갯수
+}
