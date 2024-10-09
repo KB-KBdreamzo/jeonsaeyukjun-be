7b4cd7ddc8d1021b110205e30952f73c29c3e875
JOLLA99 <yoon6148@daum.net>
2024-10-09 14:22:53 +0900

    feat : KBD-18 AWS S3 EC2 서버를 통해 업로드 다운로드 기능 구현

--- src/main/java/com/jeonsaeyukjun/jeonsaeyukjunbe/config/AppConfig.java
+++ src/main/java/com/jeonsaeyukjun/jeonsaeyukjunbe/config/AppConfig.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
@@ -18,6 +19,7 @@ import java.io.IOException;
 
 @Configuration
 @MapperScan(basePackages = {"com.jeonsaeyukjun.jeonsaeyukjunbe.**.mapper.**"})
+@PropertySource("classpath:application.properties")
 public class AppConfig {
 
     @Value("${db.url}")
